58327d91a4f65688c4ed3c522e7754a91478db16
Karen Coimes <daniz_18_8@hotmail.com>
2023-09-11 17:31:23 -0500

    Creacion Home portafolio web

---
+++ dia3/main.js
@@ -0,0 +1,7 @@
+let typed = new Typed('.auto-input', {
+    strings: ['Front-end Developer!', 'Freelancer!', 'UI Designer!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
